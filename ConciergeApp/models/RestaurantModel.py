39f3691f337e38f3db919258710b00cce974a1e0
Krystaszek Kacper <kacper.krystaszek@nokia.com>
2023-11-07 17:36:12 +0100

    add UserModel and RestaurantModel

--- ConciergeApp/models/RestaurantModel.py
+++ ConciergeApp/models/RestaurantModel.py
@@ -3,3 +3,9 @@ from django.db import models
 class RestaurantModel(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    photo_path = models.CharField(max_length=255)
+    opening_hour = models.TimeField()
+    closing_hour = models.TimeField()
+    review = models.FloatField()
+    
